9d6e7ad8b92e8da5b07fec01927cca9c95d3c618
Felipe Franciscato Honorato <fefranciscato@gmail.com>
2022-10-14 22:14:22 -0300

    Finalizando db e home

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express')
-const app = express()
-const db = require('./db/connections')
+const express    = require('express')
+const app        = express()
+const db         = require('./db/connections')
+const bodyParser = require('body-parser')
 
 const PORT = 3000
 
 app.listen(PORT, function(){
   console.log(`O Expresse está rodando na porta ${PORT}`)
 })
+
+//Body parser
+app.use(bodyParser.urlencoded({ extended: false}))
 
 // db connection
 db
@@ -15,10 +19,13 @@ db
     console.log("Conectou ao banco com sucesso!")
   })
   .catch(err => {
-    console.log("Ocorreu u erro ao conectar", err)
+    console.log("Ocorreu um erro ao conectar", err)
   })
 
 // routes
 app.get('/', (req, res) => {
   res.send("Está funcionando")
 })
+
+//jobs routes
+app.use('/jobs', require('./routes/jobs'))
---
+++ routes/jobs.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router  = express.Router()
+const Job     = require('../models/Job')
+
+//rota de teste
+router.get('/test', (req, res) => {
+  res.send('Deu certo')
+})
+
+//Adicionando job via post
+router.post('/add', (req, res) => {
+
+  let {title, salary, company, description, email, new_job} = req.body //Requisição solicita as informações que estão no body
+
+  // Insert
+  Job.create({
+    title,
+    description,
+    salary,
+    company,
+    email,
+    new_job
+  })
+  .then(() => res.redirect('/'))  //Encaminha o usuario a pagina home após inserção da vaga
+  .catch(err => console.log(err)) //Caso ocorra, vai informar o erro ao programador
+})
+
+module.exports = router
